53a2c0c85b1287d9c7625737ad503cb807bef1c0
Robert K. Carroll <robert.carroll@avanade.com>
2024-05-24 19:16:31 +0000

    update files

--- install.sh
+++ install.sh
@@ -11,5 +11,4 @@ if command -v apt-get &> /dev/null; then
 fi
 
 # Create symlinks
-ln -nfs $HOME/dotfiles/.zshrc $HOME/.zshrc
-ln -nfs $HOME/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme $HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme
+./createSymLinks.sh
