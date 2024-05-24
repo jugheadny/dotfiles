cfb03e5b44372b3813e5038731dbc376ab2d20c3
Robert K. Carroll <robert.carroll@avanade.com>
2024-05-24 19:23:35 +0000

    update

--- createSymLinks.sh
+++ createSymLinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
+files="aliases zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
 
 ##########
 
